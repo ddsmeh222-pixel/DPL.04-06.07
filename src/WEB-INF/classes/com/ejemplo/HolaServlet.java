e5aa6b565b704ae63913b754eb98d985a3ae051f
david <ddsmeh222@gmail.com>
2026-06-07 13:26:08 +0100

    Estructura completa: fuentes y artefacto v1.0

---
+++ src/WEB-INF/classes/com/ejemplo/HolaServlet.java
@@ -0,0 +1,22 @@
+package com.ejemplo;
+
+
+import java.io.IOException;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+
+public class HolaServlet extends HttpServlet {
+
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+
+        request.setAttribute("mensaje", "¡Hola de parte de David con Git!");
+        request.getRequestDispatcher("/hola.jsp").forward(request, response);
+    }
+}
